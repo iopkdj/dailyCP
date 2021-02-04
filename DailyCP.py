8c3ade6fae247889d9cbda796c63f8b88df6351f
iopkdj <60225462+iopkdj@users.noreply.github.com>
2021-02-04 22:20:07 +0800

    Update DailyCP.py

--- DailyCP.py
+++ DailyCP.py
@@ -18,7 +18,7 @@ class DailyCP:
         self.key = "b3L26XNL"  # dynamic when app update
         self.session = requests.session()
         self.host = ""
-        self.loginUrl = ""
+        self.loginUrl = "https://yangtzeu.campusphere.net/wec-portal-mobile/client"
         self.isIAPLogin = True
         self.session.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37",
